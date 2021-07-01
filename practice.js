f945752124a81161f9af0f4037314dcdc14de589
Zeru <zeruna@ad.vzwcorp.com>
2021-07-01 10:49:08 -0500

    practice

---
+++ practice.js
@@ -0,0 +1,48 @@
+
+//User Stories Use Cases
+
+// As a passenger, I want to book a flight
+// As a passenger, I want to buy bags
+// As a passenger, I want to check in my flight
+// As a passenger, I want to go through security clearance.
+
+
+
+//Airport, send the passenger their flight infromation/
+// Airport, send the passenger a confirmation of a booked ticket.
+// Airport, retrive passengers list for a flight.
+// Airport, performed a security check on passenger
+
+
+
+
+
+//Class Diagram 
+
+
+/*
+class Flight {
+   flightNumber: INTEGER
+   flightDestination: STRING
+   numberOfTicketsAvailable: STRING
+   flightStatus()
+   
+}
+
+class Passenger {
+    
+    name: String
+    customerBoughtBags = BOOLEAN
+    checkinFlight = BOOLEAN
+    passedClearance = BOOLEAN
+    flightBooked()
+}
+
+class Airport {
+    sentFlightInfromation = BOOLEAN
+    passengerSecurityCheck = BOOLEAN
+    retrivePassengerList()  
+}
+
+
+*/
